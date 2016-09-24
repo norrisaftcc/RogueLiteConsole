c4a2606e6685ddc5ad021c9cf6ee6b0ec2d06cfd
Andrew Norris <norrisd1980@gmail.com>
2016-09-24 15:51:49 -0400

    Added base project using RLNET.dll to display empty console.

--- RogueLiteConsole/RogueLiteConsole/Program.cs
+++ RogueLiteConsole/RogueLiteConsole/Program.cs
@@ -3,13 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RLNET;
 
 namespace RogueLiteConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int playerX = 25;
+        private static int playerY = 25;
+        private static RLRootConsole rootConsole;
+
+        public static void Main()
         {
+            RLSettings settings = new RLSettings();
+            settings.BitmapFile = "ascii_8x8.png";
+            settings.CharWidth = 8;
+            settings.CharHeight = 8;
+            settings.Width = 60;
+            settings.Height = 40;
+            settings.Scale = 1f;
+            settings.Title = "RLNET Sample";
+            settings.WindowBorder = RLWindowBorder.Resizable;
+            settings.ResizeType = RLResizeType.ResizeCells;
+            settings.StartWindowState = RLWindowState.Normal;
+
+            rootConsole = new RLRootConsole(settings);
+            rootConsole.Update += rootConsole_Update;
+            rootConsole.Render += rootConsole_Render;
+            rootConsole.OnLoad += rootConsole_OnLoad;
+            rootConsole.Run();
+
+        }
+
+        static void rootConsole_OnLoad(object sender, EventArgs e)
+        {
+            //rootConsole.SetWindowState(RLWindowState.Fullscreen);
+        }
+
+        static void rootConsole_Update(object sender, UpdateEventArgs e)
+        {
+            RLKeyPress keyPress = rootConsole.Keyboard.GetKeyPress();
+            if (keyPress != null)
+            {
+                if (keyPress.Key == RLKey.Up)
+                    playerY--;
+                else if (keyPress.Key == RLKey.Down)
+                    playerY++;
+                else if (keyPress.Key == RLKey.Left)
+                    playerX--;
+                else if (keyPress.Key == RLKey.Right)
+                    playerX++;
+                if (keyPress.Key == RLKey.Escape)
+                    rootConsole.Close();
+            }
+        }
+
+        static void rootConsole_Render(object sender, UpdateEventArgs e)
+        {
+            rootConsole.Clear();
+
+            rootConsole.Print(1, 1, "Hello World!", RLColor.White);
+
+            rootConsole.SetChar(playerX, playerY, '@');
+
+            int color = 1;
+            if (rootConsole.Mouse.LeftPressed)
+            {
+                color = 4;
+            }
+            rootConsole.SetBackColor(rootConsole.Mouse.X, rootConsole.Mouse.Y, RLColor.CGA[color]);
+
+            rootConsole.Draw();
         }
     }
+
 }
