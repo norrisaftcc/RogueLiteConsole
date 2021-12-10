a4bff5a51896a1a11b535f33f2a7412a058aa5be
norrisaftcc <norrisa@faytechcc.edu>
2021-12-09 22:48:09 -0500

    initial roguepy tutorial from RLT

---
+++ roguepy/actions.py
@@ -0,0 +1,15 @@
+#actions.py
+class Action:
+    pass
+
+
+class EscapeAction(Action):
+    pass
+
+
+class MovementAction(Action):
+    def __init__(self, dx: int, dy: int):
+        super().__init__()
+
+        self.dx = dx
+        self.dy = dy
